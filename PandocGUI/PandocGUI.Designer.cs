4e552ba6aa7c500ade5d843d7ddb72816dae3f64
xiang ji <smartjx@hotmail.com>
2017-03-23 14:18:36 +0800

    first commit

---
+++ PandocGUI/PandocGUI.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace PandocGUI
+{
+    partial class PandocGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.selectSource = new System.Windows.Forms.Button();
+            this.selectOutput = new System.Windows.Forms.Button();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.convert = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源文件:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "输出:";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(63, 30);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(170, 22);
+            this.txtSource.TabIndex = 2;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(63, 79);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(170, 22);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // selectSource
+            // 
+            this.selectSource.Location = new System.Drawing.Point(250, 25);
+            this.selectSource.Name = "selectSource";
+            this.selectSource.Size = new System.Drawing.Size(75, 32);
+            this.selectSource.TabIndex = 4;
+            this.selectSource.Text = "选择";
+            this.selectSource.UseVisualStyleBackColor = true;
+            this.selectSource.Click += new System.EventHandler(this.selectSource_Click);
+            // 
+            // selectOutput
+            // 
+            this.selectOutput.Location = new System.Drawing.Point(250, 75);
+            this.selectOutput.Name = "selectOutput";
+            this.selectOutput.Size = new System.Drawing.Size(75, 31);
+            this.selectOutput.TabIndex = 5;
+            this.selectOutput.Text = "选择";
+            this.selectOutput.UseVisualStyleBackColor = true;
+            this.selectOutput.Click += new System.EventHandler(this.selectOutput_Click);
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Items.AddRange(new object[] {
+            "html",
+            "pdf",
+            "docx"});
+            this.cbFormat.Location = new System.Drawing.Point(87, 130);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(121, 24);
+            this.cbFormat.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "输出格式:";
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(250, 123);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(75, 31);
+            this.convert.TabIndex = 8;
+            this.convert.Text = "转换";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 215);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 59);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // PandocGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 274);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbFormat);
+            this.Controls.Add(this.selectOutput);
+            this.Controls.Add(this.selectSource);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PandocGUI";
+            this.Text = "PandocGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button selectSource;
+        private System.Windows.Forms.Button selectOutput;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PandocGUI/PandocGUI.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+
+namespace PandocGUI
+{
+    public partial class PandocGUI : Form
+    {
+     private static   StringBuilder output = new StringBuilder();
+        private static int lineCount = 0;
+
+        public PandocGUI()
+        {
+            InitializeComponent();
+             txtOutput.Text = Properties.Settings.Default.OutputPath;
+        }
+
+        private async void convert_Click(object sender, EventArgs e)
+        {
+
+            var onlyFileName = System.IO.Path.GetFileName(txtSource.Text);
+            var command = txtSource.Text + " -o " +  txtOutput.Text+"\\" + onlyFileName+ "." + cbFormat.SelectedItem;
+            //runPandoc(command);
+
+        await Task.Factory.StartNew(() => runPandoc(@command),
+                                TaskCreationOptions.LongRunning);
+
+            richTextBox1.Text = output.ToString();
+          //  System.Diagnostics.Process.Start("pandoc", command);
+        }
+
+        private void selectSource_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                txtSource.Text = openFileDialog1.FileName;
+
+                
+                Properties.Settings.Default.SourceFile = openFileDialog1.FileName;
+                Properties.Settings.Default.Save();
+            }
+           
+        }
+         
+        private void selectOutput_Click(object sender, EventArgs e)
+         {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                txtOutput.Text = folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default.OutputPath = folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default.Save();
+             }
+
+        }
+
+        private string runPandoc(string command)
+        {
+
+            //  var output = "";
+            var pandocPath = new Uri(
+         System.IO.Path.GetDirectoryName(
+            System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+         ).LocalPath + "\\Pandoc\\pandoc.exe";
+
+            var process = new Process
+            {
+                StartInfo =
+                            new ProcessStartInfo(pandocPath)
+                            { RedirectStandardOutput = true,
+                                RedirectStandardError = true,
+                                UseShellExecute = false,
+                                Arguments = command,
+                                CreateNoWindow = true
+                         }
+        };
+
+ 
+            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                // Prepend line numbers to each line of the output.
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    lineCount++;
+                    output.Append("\n[" + lineCount + "]: " + e.Data);
+                  }
+            });
+
+            process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                // Prepend line numbers to each line of the output.
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    lineCount++;
+                    output.Append("\n[" + lineCount + "]: " + e.Data);
+ 
+                }
+            });
+
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+           return output.ToString();
+
+           
+
+        }
+    
+   
+}
+}
