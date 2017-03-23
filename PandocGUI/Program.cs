802d15a2e5da9cf9ff86e19830dc9657003bd26c
xiang ji <smartjx@hotmail.com>
2017-03-23 14:54:59 +0800

    test push

--- PandocGUI/Program.cs
+++ PandocGUI/Program.cs
@@ -11,7 +11,7 @@ namespace PandocGUI
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]//we
+        [STAThread]//we//test github push
         static void Main()
         {
             Application.EnableVisualStyles();
